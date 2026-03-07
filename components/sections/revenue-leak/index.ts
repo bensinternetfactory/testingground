7ea907a94c06907abd5d6485911091435b2797fc
BenFranzoso <bensinternetfactory@gmail.com>
2026-03-07 14:51:43 -0500

    Remove unused legacy sections

--- components/sections/revenue-leak/index.ts
+++
@@ -1,8 +0,0 @@
-export { FinancingCards } from "./FinancingCards";
-export { GuideBuilder } from "./GuideBuilder";
-export { FeaturedPrograms } from "./FeaturedPrograms";
-export { CalculatorCTA } from "./CalculatorCTA";
-export { TestimonialsSection } from "./TestimonialsSection";
-export { FinalCTA } from "./FinalCTA";
-export { LegalSection } from "./LegalSection";
-export { FooterSection } from "./FooterSection";
